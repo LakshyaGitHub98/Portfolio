c36a528e3075781d08ce98e80aad33cb5df750c9
LakshyaGitHub98 <laktripathi9839@gmail.com>
2026-09-07 00:34:26 +0530

    perf: remove redundant fontsource CSS imports in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,6 @@ import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 
-import "@fontsource/ibm-plex-sans/400.css";
-import "@fontsource/ibm-plex-sans/500.css";
-import "@fontsource/ibm-plex-sans/600.css";
-import "@fontsource/ibm-plex-mono/400.css";
-import "@fontsource/ibm-plex-mono/500.css";
-
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-display",
